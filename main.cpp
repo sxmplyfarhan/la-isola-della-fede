f7bdc49bd24f3f995ed5f658140edeedb6f08930
sxmplyfarhan <sxmplyfarhan@gmail.com>
2025-12-28 16:42:42 +0100

    Aggiornamento di main.cpp. Aggiunta di nuove impostazioni e introduzione a gioco

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
@@ -20,20 +21,53 @@ void clearScreen() {
 
 string playerName ="Giocatore";
 string difficulty = "Normale";
+string infront = "w";
+string sinistra = "a";
+string destra ="d";
+string indietro = "s";
+string inventario = "e";
+string pieta = "r";
+string scappare = "l";
+string attaccare = "q";
+
 
 void loadSettings() {
     ifstream in("impostazioni.txt"); //legge
     if (in) {
-        getline(in, playerName);
-        getline(in, difficulty);
+        string line;
+        while (getline(in, line)) {
+            size_t pos = line.find(" = ");
+            if (pos != string::npos) {
+                string key = line.substr(0, pos);
+                string value = line.substr(pos + 3); // Dopo " = "
+                if (key == "playerName") playerName = value;
+                else if (key == "difficulty") difficulty = value;
+                else if (key == "infront") infront = value;
+                else if (key == "sinistra") sinistra = value;
+                else if (key == "destra") destra = value;
+                else if (key == "indietro") indietro = value;
+                else if (key == "inventario") inventario = value;
+                else if (key == "pieta") pieta = value;
+                else if (key == "scappare") scappare = value;
+                else if (key == "attaccare") attaccare = value;
+            }
+        }
         in.close();
     }
 }
 
 void saveSettings() {
-    ofstream out ("impostazioni.txt"); //scrive
-    out << playerName << "\n";
-    out << difficulty << "\n";
+    ofstream out("impostazioni.txt"); //scrive
+    out << "playerName = " << playerName << "\n";
+    out << "difficulty = " << difficulty << "\n";
+    out << "infront = " << infront << "\n";
+    out << "sinistra = " << sinistra << "\n";
+    out << "destra = " << destra << "\n";
+    out << "indietro = " << indietro << "\n";
+    out << "inventario = " << inventario << "\n";
+    out << "pieta = " << pieta << "\n";
+    out << "scappare = " << scappare << "\n";
+    out << "attaccare = " << attaccare << "\n";
     out.close();
 }
 
@@ -96,6 +130,21 @@ void printCreditsTitle() {
     cout << "\033[0m\n";
 }
 
+void printIntroductionTitle(){
+    cout << "\033[35m";
+    cout << "┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐\n";
+    cout << "│  ███              █████                           █████                        ███                              │\n";
+    cout << "│ ░░░              ░░███                           ░░███                        ░░░                               │\n";
+    cout << "│ ████  ████████   ███████   ████████   ██████   ███████  █████ ████  █████████ ████   ██████  ████████    ██████ │\n";
+    cout << "│░░███ ░░███░░███ ░░░███░   ░░███░░███ ███░░███ ███░░███ ░░███ ░███  ░█░░░░███ ░░███  ███░░███░░███░░███  ███░░███│\n";
+    cout << "│ ░███  ░███ ░███   ░███     ░███ ░░░ ░███ ░███░███ ░███  ░███ ░███  ░   ███░   ░███ ░███ ░███ ░███ ░███ ░███████ │\n";
+    cout << "│ ░███  ░███ ░███   ░███ ███ ░███     ░███ ░███░███ ░███  ░███ ░███    ███░   █ ░███ ░███ ░███ ░███ ░███ ░███░░░  │\n";
+    cout << "│ █████ ████ █████  ░░█████  █████    ░░██████ ░░████████ ░░████████  █████████ █████░░██████  ████ █████░░██████ │\n";
+    cout << "│░░░░░ ░░░░ ░░░░░    ░░░░░  ░░░░░      ░░░░░░   ░░░░░░░░   ░░░░░░░░  ░░░░░░░░░ ░░░░░  ░░░░░░  ░░░░ ░░░░░  ░░░░░░  │\n";
+    cout << "└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n";
+    cout << "\033[0m\n";
+}
+
 void showCredits() {
     clearScreen();
     printCreditsTitle();
@@ -112,8 +161,16 @@ void showSettings() {
 
         cout << "\n1. Nome giocatore: " << playerName << endl;
         cout << "2. Difficolta: " <<  difficulty << endl;
-        cout << "3. Salva " << endl;
-        cout << "4. Esci" << endl;
+        cout << "3. Avanti: " <<  infront << endl;
+        cout << "4. Sinistra: " <<  sinistra << endl;
+        cout << "5. Destra: " <<  destra << endl;
+        cout << "6. Indietro: " <<  indietro << endl;
+        cout << "7. Inventario: " <<  inventario << endl;
+        cout << "8. Pietá: " <<  pieta << endl;
+        cout << "9. Scappare: " <<  scappare << endl;
+        cout << "10. Attaccare: " <<  attaccare << endl;
+        cout << "11. Salva " << endl;
+        cout << "12. Esci" << endl;
         
         cout << "\033[35m";
         cout << "\n>> ";
@@ -124,8 +181,7 @@ void showSettings() {
         if(choice == 1) {
             cout << "Inserisci nome: ";
             cin.ignore();
-            getline(cin, playerName);
-            
+            getline(cin, playerName);   
         } else if (choice == 2) {
             if (difficulty == "Facile") {
                 difficulty = "Normale"; 
@@ -133,6 +189,7 @@ void showSettings() {
                 cout << "Premi invio per continuare.. ";
                 cin.ignore();
                 cin.get();
+                
             } else if (difficulty == "Normale") {
                 difficulty = "Difficile";
                 cout << "Difficolta' impostata su: " << difficulty << endl;
@@ -147,19 +204,92 @@ void showSettings() {
                 cin.get();
             }
         } else if (choice == 3) {
+            cout << "Inserisci tasto da assegnare a 'Avanti': ";
+            cin.ignore();
+            getline(cin, infront);
+        } else if (choice == 4) {
+            cout << "Inserisci tasto da assegnare a 'Sinistra': ";
+            cin.ignore();
+            getline(cin, sinistra);
+        }else if (choice == 5) {
+            cout << "Inserisci tasto da assegnare a 'Destra': ";
+            cin.ignore();
+            getline(cin, destra);
+        }else if (choice == 6) {
+            cout << "Inserisci tasto da assegnare a 'Indiestro': ";
+            cin.ignore();
+            getline(cin, indietro);
+        }else if (choice == 7) {
+            cout << "Inserisci tasto da assegnare a 'Inventario': ";
+            cin.ignore();
+            getline(cin, inventario);
+        }else if (choice == 8) {
+            cout << "Inserisci tasto da assegnare a 'Pietà': ";
+            cin.ignore();
+            getline(cin, pieta);
+        }else if (choice == 9) {
+            cout << "Inserisci tasto da assegnare a 'Scappare': ";
+            cin.ignore();
+            getline(cin, scappare);
+        }else if (choice == 10) {
+            cout << "Inserisci tasto da assegnare a 'Attaccare': ";
+            cin.ignore();
+            getline(cin, attaccare);
+        } else if (choice == 11) {
             saveSettings();
             cout << "Impostazioni Salvate!\n" <<"Premi invio per continuare.. ";
             cin.ignore();
             cin.get();
-        } else if (choice == 4) {
+        } else if (choice == 12) {
             break;
-        } else {
-            cout << "Scelta non valida. \n Premi invio per riprovare";
-            cin.ignore();
-            cin.get();
         }
 
-        } 
+    }  
+}
+
+void introduzioneGioco(){
+    while (true) {
+    clearScreen();
+    printIntroductionTitle();
+    cout << "\033[97;1m";
+    cout << "Puoi muoverti facilmente nel mondo usanto i tasti: \n";
+    cout << "\033[0m";
+    cout << "\nCon il pulsante " << "\033[35m" << infront << "\033[0m" << " puoi andare avanti.\n";
+    cout << "Con il pulsante " << "\033[35m" << sinistra << "\033[0m" << " puoi andare a sinistra.\n";
+    cout << "Con il pulsante " << "\033[35m" << destra << "\033[0m" << " puoi andare a destra.\n";
+    cout << "Con il pulsante " << "\033[35m" << indietro << "\033[0m" <<" puoi andare indietro.\n";
+
+    cout << "\033[90m";
+    cout << "\nPremi invio per continuare...";
+    cout << "\033[0m";
+    
+    cin.ignore();
+    cin.get();
+    clearScreen();
+    printIntroductionTitle();
+    cout << "\033[97;1m";
+    cout << "Nel gioco ci sono fare cose che puoi fare: \n";
+    cout << "\033[97;0m";
+
+    cout << "\nCon il pulsante " << "\033[35m" << inventario << "\033[0m" << " puoi aprire lo zaino.\n";
+    cout << "Con il pulsante " << "\033[35m" << pieta << "\033[0m" << " puoi lacsiare un nemico vivere.\n";
+    cout << "Con il pulsante " << "\033[35m" << scappare << "\033[0m" << " puoi scappare da una battaglia per curarti.\n";
+    cout << "Con il pulsante " << "\033[35m" << attaccare << "\033[0m" <<" puoi attaccare un nemico.\n";
+
+    cout << "\033[90m";
+    cout << "\nPremi invio per continuare...";
+    cout << "\033[0m";
+    cin.ignore();
+    cin.get();
+    break;
+    }
+}
+
+void introduzioneStoria(){
+    clearScreen();
+    cout << "funziona?";
+    cin.ignore();
+    cin.get();
 }
 
 int main() {
@@ -182,11 +312,8 @@ int main() {
         int choice;
         cin >> choice;
         if (choice == 1) {
-            clearScreen();
-            cout << "Avvio del gioco per " << playerName << " In modalita' " << difficulty << "..." << endl;
-            cout << "premi invio per per ritornare indietro";
-            cin.ignore();
-            cin.get(); 
+            introduzioneGioco();
+            introduzioneStoria();
         } else if (choice == 2) {
             showCredits();
         } else if (choice == 3) {
