4b1e89d8f2e8828b458a1ca79fc9cea5839cad3d
sxmplyfarhan <sxmplyfarhan@gmail.com>
2026-01-02 18:55:19 +0100

    Pulito un po il codice e sistemato delle cose

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@
 #endif
 using namespace std;
 
-
-// Su linux/macos si usa il comando clear per pulire il schermo
-//  ma su windows si usa cls
-
 void clearScreen() {
     #if defined(_WIN32) || defined(_WIN64)
         system("cls");
@@ -19,9 +15,13 @@ void clearScreen() {
     #endif
 }
 
+string mobs1[3] = {"Scimmia", "Cane", "Goblin"};
+string mobs2[3] = {"Pipistrello", "Farfalla", "Ragno"};
+string mobs3[3] = {"Centauro", "Mietitore", "Fenice"};
+string inventariospace[30];
 string playerName ="Giocatore";
 string difficulty = "Normale";
-string infront = "w";
+string infront = "w"; 
 string sinistra = "a";
 string destra ="d";
 string indietro = "s";
@@ -30,6 +30,55 @@ string pieta = "r";
 string scappare = "l";
 string attaccare = "q";
 
+double vita = 5;
+int nemiciUccisi = 0;
+int livello = 1;
+
+void livelloEsperienza() {
+    if (difficulty == "Normale" ) {
+            if (nemiciUccisi >= 3) {
+                livello++;
+                vita = vita + 2.5;
+                cout << "Congratuziolazioni. Sei arrivato al livello " << livello << ". Adesso hai " << vita << "vita";
+            }
+    } else if (difficulty == "Facile") {
+                if (nemiciUccisi >= 2) {
+                    livello++;
+                    vita = vita + 3.5;
+                    cout << "Congratuziolazioni. Sei arrivato al livello " << livello << ". Adesso hai " << vita << "vita";
+            }
+    } else if (difficulty == "Difficile") {
+                if (nemiciUccisi >= 5) {
+                    livello++;
+                    vita = vita + 1.5;
+                    cout << "Congratuziolazioni. Sei arrivato al livello " << livello << ". Adesso hai " << vita << "vita";
+            }
+    }
+}
+
+void combattimento(int livello, int &uccisioni, string mobs[3]){
+
+    string scelta;
+    string mob = mobs[rand() % 3];
+    if (mob == mobs2[1]){
+        cout << "Sei stato fortunanto, la farfalla ti cura!" << endl;
+    }
+    else{
+        cout << "\nHai incontrato : " << mob << "!" << endl;
+        cout << "Vuoi attaccare (premi 'q'), essere misericordioso (premi 'r') o scappare (premi 'l')? ";
+        cin >> scelta;
+        if (scelta == attaccare) {
+            cout << "Hai attaccato : " << mob << " e lo hai sconfitto!" << endl;
+            uccisioni++;
+        } else if (scelta == pieta) {
+            cout << "Sei stato misericordioso con : " << mob << " e lo hai lasciato andare.Tuttavia prendi del danno :(" << endl;
+        } else if (scelta == scappare) {
+        cout << "Sei scappato da : " << mob << " per curarti." << endl;
+        } else {
+            cout << "Scelta non valida. " << mob << " ti ha attaccato mentre eri indeciso!" << endl;
+        }
+    }
+}
 
 void loadSettings() {
     ifstream in("impostazioni.txt"); //legge
@@ -37,7 +86,7 @@ void loadSettings() {
         string line;
         while (getline(in, line)) {
             size_t pos = line.find(" = ");
-            if (pos != string::npos) {
+            if (pos != string::npos) { 
                 string key = line.substr(0, pos);
                 string value = line.substr(pos + 3); // Dopo " = "
                 if (key == "playerName") playerName = value;
@@ -56,6 +105,47 @@ void loadSettings() {
     }
 }
 
+void loadInventory() {
+    ifstream in("inventario.txt"); //legge
+    if (in) {
+        string line;
+        while (getline(in, line)) {
+            size_t pos = line.find(" = ");
+            if (pos != string::npos) { 
+                string key = line.substr(0, pos);
+                string value = line.substr(pos + 3); // Dopo " = "
+                if (key == "oggetto") inventariospace[0] = value;
+                else if (key == "oggetto") inventariospace[1] = value;
+                else if (key == "oggetto") inventariospace[2] = value;
+                else if (key == "oggetto") inventariospace[3] = value;
+                else if (key == "oggetto") inventariospace[4] = value;
+                else if (key == "oggetto") inventariospace[5] = value;
+                else if (key == "oggetto") inventariospace[6] = value;
+                else if (key == "oggetto") inventariospace[7] = value;
+                else if (key == "oggetto") inventariospace[8] = value;
+                else if (key == "oggetto") inventariospace[9] = value;
+                else if (key == "oggetto") inventariospace[10] = value;
+                else if (key == "oggetto") inventariospace[11] = value;
+                else if (key == "oggetto") inventariospace[12] = value;
+                else if (key == "oggetto") inventariospace[13] = value;
+                else if (key == "oggetto") inventariospace[14] = value;
+                else if (key == "oggetto") inventariospace[15] = value;
+                else if (key == "oggetto") inventariospace[16] = value;
+                else if (key == "oggetto") inventariospace[17] = value;
+                else if (key == "oggetto") inventariospace[18] = value;
+                else if (key == "oggetto") inventariospace[19] = value;
+                else if (key == "oggetto") inventariospace[20] = value;
+                else if (key == "oggetto") inventariospace[21] = value;
+                else if (key == "oggetto") inventariospace[22] = value;
+                else if (key == "oggetto") inventariospace[23] = value;
+                else if (key == "oggetto") inventariospace[24] = value;
+                else if (key == "oggetto") inventariospace[25] = value;
+            }
+        }
+        in.close();
+    }
+}
+
 void saveSettings() {
     ofstream out("impostazioni.txt"); //scrive
     out << "playerName = " << playerName << "\n";
@@ -68,6 +158,37 @@ void saveSettings() {
     out << "pieta = " << pieta << "\n";
     out << "scappare = " << scappare << "\n";
     out << "attaccare = " << attaccare << "\n";
+    out.close();
+}
+
+void saveInventory() {
+    ofstream out("inventario.txt"); //scrive
+    out << "oggetto = " << inventariospace[0] << "\n";
+    out << "oggetto = " << inventariospace[1] << "\n";
+    out << "oggetto = " << inventariospace[2] << "\n";
+    out << "oggetto = " << inventariospace[3] << "\n";
+    out << "oggetto = " << inventariospace[4] << "\n";
+    out << "oggetto = " << inventariospace[5] << "\n";
+    out << "oggetto = " << inventariospace[6] << "\n";
+    out << "oggetto = " << inventariospace[7] << "\n";
+    out << "oggetto = " << inventariospace[8] << "\n";
+    out << "oggetto = " << inventariospace[9] << "\n";
+    out << "oggetto = " << inventariospace[10] << "\n";
+    out << "oggetto = " << inventariospace[11] << "\n";
+    out << "oggetto = " << inventariospace[12] << "\n";
+    out << "oggetto = " << inventariospace[13] << "\n";
+    out << "oggetto = " << inventariospace[14] << "\n";
+    out << "oggetto = " << inventariospace[15] << "\n";
+    out << "oggetto = " << inventariospace[16] << "\n";
+    out << "oggetto = " << inventariospace[17] << "\n";
+    out << "oggetto = " << inventariospace[18] << "\n";
+    out << "oggetto = " << inventariospace[19] << "\n";
+    out << "oggetto = " << inventariospace[20] << "\n";
+    out << "oggetto = " << inventariospace[21] << "\n";
+    out << "oggetto = " << inventariospace[22] << "\n";
+    out << "oggetto = " << inventariospace[23] << "\n";
+    out << "oggetto = " << inventariospace[24] << "\n";
+    out << "oggetto = " << inventariospace[25] << "\n";
     out.close();
 }
 
@@ -145,6 +266,69 @@ void printIntroductionTitle(){
     cout << "\033[0m\n";
 }
 
+void Input(){
+    cout << "\033[35m";
+    cout << "\n>> ";
+    cout << "\033[0m";
+}
+
+void Invio(){
+    cout << "\033[90m";
+    cout << "\nPremi invio per continuare...";
+    cout << "\033[0m";
+}
+
+void displayAsciiArt(const string& filename) {
+    ifstream file(filename);
+    char c;
+    while (file.get(c)) {
+        cout << c;
+    }
+}
+
+void displayBoss() {
+    displayAsciiArt("ASCII/boss.txt");
+}
+void displayVane() {
+    displayAsciiArt("ASCII/cane.txt");
+}
+void displayCentauro() {
+    displayAsciiArt("ASCII/isola.txt");
+}
+void displayDragon() {
+    displayAsciiArt("ASCII/dragoboss.txt");
+}
+void displayFarfalla() {
+    displayAsciiArt("ASCII/farfalla.txt");
+}
+void displayFenice() {
+    displayAsciiArt("ASCII/fenice.txt");
+}
+void displayGoblin() {
+    displayAsciiArt("ASCII/goblin.txt");
+}
+void displayGoblinKing() {
+    displayAsciiArt("ASCII/goblinking.txt");
+}
+void displayReaper() {
+    displayAsciiArt("ASCII/grimreaper.txt");
+}
+void displayIsola() {
+    displayAsciiArt("ASCII/isola.txt");
+}
+void displayBat() {
+    displayAsciiArt("ASCII/pipistrelli.txt");
+}
+void displayRagni() {
+    displayAsciiArt("ASCII/ragni.txt");
+}
+void displayScimmie() {
+    displayAsciiArt("ASCII/scimmie.txt");
+}
+void displayVampireBoss() {
+    displayAsciiArt("ASCII/vampiroboss.txt");
+}
+
 void showCredits() {
     clearScreen();
     printCreditsTitle();
@@ -156,9 +340,9 @@ void showCredits() {
 
 void showSettings() {
     while(true) {
+
         clearScreen();
         printSettingsTitle();
-
         cout << "\n1. Nome giocatore: " << playerName << endl;
         cout << "2. Difficolta: " <<  difficulty << endl;
         cout << "3. Avanti: " <<  infront << endl;
@@ -171,10 +355,7 @@ void showSettings() {
         cout << "10. Attaccare: " <<  attaccare << endl;
         cout << "11. Salva " << endl;
         cout << "12. Esci" << endl;
-        
-        cout << "\033[35m";
-        cout << "\n>> ";
-        cout << "\033[0m";
+        Input();
 
         int choice;
         cin >> choice;
@@ -186,20 +367,19 @@ void showSettings() {
             if (difficulty == "Facile") {
                 difficulty = "Normale"; 
                 cout << "Difficolta' impostata su: " << difficulty << endl;
-                cout << "Premi invio per continuare.. ";
+                Invio();
                 cin.ignore();
                 cin.get();
-                
             } else if (difficulty == "Normale") {
                 difficulty = "Difficile";
                 cout << "Difficolta' impostata su: " << difficulty << endl;
-                cout << "Premi invio per continuare.. ";
+                Invio();
                 cin.ignore();
                 cin.get();
             } else {
                 difficulty = "Facile";
                 cout << "Difficolta' impostata su: " << difficulty << endl;
-                cout << "Premi invio per continuare.. ";
+                Invio();
                 cin.ignore();
                 cin.get();
             }
@@ -237,7 +417,9 @@ void showSettings() {
             getline(cin, attaccare);
         } else if (choice == 11) {
             saveSettings();
-            cout << "Impostazioni Salvate!\n" <<"Premi invio per continuare.. ";
+            saveInventory();
+            cout << "Impostazioni Salvate!\n";
+            Invio();
             cin.ignore();
             cin.get();
         } else if (choice == 12) {
@@ -258,10 +440,7 @@ void introduzioneGioco(){
     cout << "Con il pulsante " << "\033[35m" << sinistra << "\033[0m" << " puoi andare a sinistra.\n";
     cout << "Con il pulsante " << "\033[35m" << destra << "\033[0m" << " puoi andare a destra.\n";
     cout << "Con il pulsante " << "\033[35m" << indietro << "\033[0m" <<" puoi andare indietro.\n";
-
-    cout << "\033[90m";
-    cout << "\nPremi invio per continuare...";
-    cout << "\033[0m";
+    Invio();
     
     cin.ignore();
     cin.get();
@@ -270,26 +449,48 @@ void introduzioneGioco(){
     cout << "\033[97;1m";
     cout << "Nel gioco ci sono fare cose che puoi fare: \n";
     cout << "\033[97;0m";
-
     cout << "\nCon il pulsante " << "\033[35m" << inventario << "\033[0m" << " puoi aprire lo zaino.\n";
     cout << "Con il pulsante " << "\033[35m" << pieta << "\033[0m" << " puoi lacsiare un nemico vivere.\n";
     cout << "Con il pulsante " << "\033[35m" << scappare << "\033[0m" << " puoi scappare da una battaglia per curarti.\n";
     cout << "Con il pulsante " << "\033[35m" << attaccare << "\033[0m" <<" puoi attaccare un nemico.\n";
+    Invio();
+    cin.get();
 
-    cout << "\033[90m";
-    cout << "\nPremi invio per continuare...";
-    cout << "\033[0m";
-    cin.ignore();
+    clearScreen();
+    printIntroductionTitle();
+    cout << "\033[97;1m";
+    cout << "Informazioni sul gioco\n";
+    cout << "\033[97;0m";
+    cout << "\nInizi con 5 cuori e puoi arrivare fino a 20.\n";
+    cout << "I nemici fanno piu danno quando hai piu cuori.\n";
+    cout << "L'inventario non ha limiti di spazio.\n";
+    cout << "Ci sono 4 livelli: 3 con mostri e boss, e l'ultimo con il boss finale.\n";
+    cout << "I boss possono dropare amuleti e danno sempre una chiave.\n";
+    cout << "Puoi portare armi e pozioni di cura.\n";
+    cout << "Le tue scelte influenzano il gioco.\n";
+    cout << "\033[97;1m";
+    cout << "\nDopo che premi INVIO. Iniziera il gioco reale...";
+    cout << "\033[97;0m";
+    Invio();
+    cin.get();
+
+    clearScreen();
+    displayIsola();
+    cout << "\nCiao, so che sei un po stordito poiche non ricordi nulla e non sai perche su un'isola in mezzo al nulla ora ti raccontero tutto.\n";
+    cout << "Tutto era iniziato nella tua camera mentre stavi guardando l'ultimo episodio di Twisted Metal/The Witcher mentre intorno a te cera un silenzio tombale.\n";
+    cout << "All'improvviso sentissi un tonfo proveniente dalla finestra e cercassi di capire l'origine.\n";
+    Invio();
+    cin.get();
+
+    clearScreen();
+    displayIsola();
+    cout << "\nAll'improvviso si ruppe la finestra e dopo quatro rapitori entrarono all'interno molto velocemente, tu provassi a reagire ma eri in svantaggio numerico quindi ti rapirono.\n";
+    cout << "Quando ti svegliasti eri su un'sola legato con i quatro ladri davanti a te, tu trovassi un buon momento in cui erano distratti e scappasti ma dopo un po svenni per la stanchewzza.\n";
+    cout << "Per questo ti trovi qui.\n";
+    Invio();
     cin.get();
     break;
     }
-}
-
-void introduzioneStoria(){
-    clearScreen();
-    cout << "funziona?";
-    cin.ignore();
-    cin.get();
 }
 
 int main() {
@@ -304,16 +505,12 @@ int main() {
         cout << "2. Crediti " << endl;
         cout << "3. impostazioni" << endl;
         cout << "4. Esci" << endl; 
-
-        cout << "\033[35m";
-        cout << "\n>> ";
-        cout << "\033[0m";
+        Input();
 
         int choice;
         cin >> choice;
         if (choice == 1) {
             introduzioneGioco();
-            introduzioneStoria();
         } else if (choice == 2) {
             showCredits();
         } else if (choice == 3) {
@@ -323,7 +520,8 @@ int main() {
             cout << "Arrivederci, ritorna da noi amore <3" << endl;
             break;
         } else {
-            cout << "Scelta non valida. Premi invio per riprovare"; 
+            cout << "Scelta non valida."; 
+            Invio();
             cin.ignore();
             cin.get();
         }
